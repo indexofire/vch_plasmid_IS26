596a90d7efb42eb068c61c9596a8e247dda03125
Mark Renton <indexofire@gmail.com>
2015-04-24 16:56:32 +0800

    update data

--- diagram/plasmid.py
+++ diagram/plasmid.py
@@ -3,26 +3,29 @@ from reportlab.lib.units import cm
 from Bio.Graphics import GenomeDiagram
 from Bio import SeqIO
 
-record = SeqIO.read("../sequences/0001.gb", "genbank")
 
-gd_diagram = GenomeDiagram.Diagram("Vibrio cholerae  plasmid 2012EL-2176")
-gd_track_for_features = gd_diagram.new_track(1, name="Annotated Features")
-gd_feature_set = gd_track_for_features.new_set()
+genomes = ['0001', '0005', '0007', '0008', '0009', '0010', '0011', '0014', '0015', '0016',
+    '0017','0018', '0019', '0020', '0021', '0022', '0024', '0035']
+gd_diagram = GenomeDiagram.Diagram("Vibrio cholerae plasmid 2012EL-2176")
+max_len = 0
 
-for feature in record.features:
-   # print type(feature.qualifiers.get('product'))
-    if feature.type != "gene":
-        #print type(feature.qualifiers.get('product'))
-        if isinstance(feature.qualifiers.get('product'), list) and feature.qualifiers.get('product')[0] == 'transposase':
-            #print feature.qualifiers.get('product')
-            if len(gd_feature_set) % 2 == 0:
-                color = colors.blue
-            else:
-                color = colors.lightblue
-            #gd_feature_set.add_feature(feature, color=color, label=True)
-            gd_feature_set.add_feature(feature, label=True, sigil="ARROW", color="brown", arrowshaft_height=0.2, name=feature.qualifiers.get('product')[0])
+for genome in genomes:
+    record = SeqIO.read("../sequences/%s.gb" % genome, "genbank")
+    max_len = max(max_len, len(record))
+    gd_track_for_features = gd_diagram.new_track(1, greytrack=True, start=0, end=len(record), name=record.name)
+    gd_feature_set = gd_track_for_features.new_set()
+    for feature in record.features:
 
-gd_diagram.draw(format="linear", orientation="landscape", pagesize='A4', fragments=30, start=0, end=len(record))
+        if feature.type != "gene":
+            if isinstance(feature.qualifiers.get('product'), list) and 'transposase' in feature.qualifiers.get('product')[0] :
+
+                if len(gd_feature_set) % 2 == 0:
+                    color = '#44ACAC'
+                else:
+                    color = '#EC5503'
+                gd_feature_set.add_feature(feature, label=False, sigil="ARROW", color=color,
+                    arrowshaft_height=0.25, name=feature.qualifiers.get('product')[0],
+                    label_position="middle", label_angle=45)
+
+gd_diagram.draw(format="linear", orientation="landscape", pagesize=(40*cm, 80*cm), fragments=1, start=0, end=max_len)
 gd_diagram.write("linear.pdf", "PDF")
-#gd_diagram.write("plasmid_linear.eps", "EPS")
-#gd_diagram.write("plasmid_linear.svg", "SVG")
